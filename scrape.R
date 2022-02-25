2aa2d4932a974e59fe2b09429b685cfcc7f22e4e
mca91 <martin.emwikts@googlemail.com>
2022-02-25 11:02:00 +0100

    some modification and merged code into a single .R file. Also added results for first run

--- scrape.R
+++ scrape.R
@@ -1,12 +1,28 @@
 
+library(rvest)
+library(tidyverse)
+library(lubridate)
 
+# I use German UFT-8 locale for Linux-based OS and "German_Germany.1252" on Windows.
+# !!Note that using the system default may fail ( check Sys.getlocale("LC_TIME") )
+# because match dates / times are in German format and we need a corresponding locale for lubridate
+# to parse correctly to date_time format!!
+
+# Here I set the locale globally. Other option is to provide a locale
+# to the match data scraping functions.
+Sys.setlocale(locale = "de_DE.UTF-8") 
+
+
+############################################ functions ############################################
+
+# function for scraping match report _URLs_ for provided seasons.
 all_spielbericht_urls <- function(
-  spielplaene, 
+  spielplaene,  # needs to be a named nested list, see body
   sleep = 1, 
   base_url = "https://www.weltfussball.de", 
   dir = getwd(),
   save_result = TRUE
-  ) {
+) {
   
   matchdays <- 1:length(spielplaene) 
   league_names <- names(spielplaene)
@@ -19,7 +35,7 @@ all_spielbericht_urls <- function(
       ~ {
         cat("getting spielbericht URLs for '", league_names[x] , "'", "matchday", .x, "\n") 
         get_spielbericht_urls(spielplaene[[x]][.x], sleep = sleep) %>% 
-        paste0(base_url, .) 
+          paste0(base_url, .) 
       }
     )
   )
@@ -31,13 +47,60 @@ all_spielbericht_urls <- function(
 }  
 
 
+# function for scraping results of _a single match_
+get_match_data <- function(
+  link, 
+  sleep = 1, 
+  locale = "de_DE.UTF-8"
+  ) {
+  
+  # pause
+  Sys.sleep(sleep)
+  
+  # parse match result page
+  page <- read_html(link)
+  
+  # home cards
+  home_cards <- page %>% html_elements("table .standard_tabelle") %>% .[1] %>% html_elements("tr > td > img") %>% html_attr("alt")
+  
+  # away cards
+  away_cards <- page %>% html_elements("table .standard_tabelle") %>% .[2] %>% html_elements("tr > td > img") %>% html_attr("alt")
+  
+  # obtain and return tibble with interesting variables
+  tibble(
+    date = page %>% html_element("th:nth-child(2)") %>% html_text2() %>% str_replace("\\n", " ") %>% parse_date_time(orders = "%A, %d. %B %Y %H:%M", locale = locale, tz = "Europe/Berlin"),  
+    league = str_extract(link, pattern = "(?<=spielbericht/).+(?=-\\d{4}-\\d{4})"),
+    season = str_extract(link, pattern = "\\d{4}-\\d{4}"),
+    matchday = page %>% html_elements("select") %>% .[3] %>% html_elements("option[selected='selected']") %>% html_text() %>% parse_number(),
+    home = page %>% html_node("th:nth-child(1) a") %>% html_text(),
+    away = page %>% html_node("th+ th a") %>% html_text(),
+    outcome = page %>% html_node(".resultat") %>% html_text(),
+    ref = page %>% html_node("tr:nth-child(3) .dunkel~ .dunkel+ .dunkel") %>% html_text(),
+    att = page %>% html_node("br+ .standard_tabelle tr:nth-child(2) .dunkel~ .dunkel+ .dunkel") %>% html_text() %>% str_remove(pattern = "\\.") %>% str_trim(),
+    yellow_home = sum(home_cards == "gelb"),
+    yellow_away = sum(away_cards == "gelb"),
+    red_home = sum(home_cards == "rot"),
+    red_away = sum(away_cards == "rot"),
+    redyellow_home = sum(home_cards == "gelb-rot"),
+    redyellow_away = sum(away_cards == "gelb-rot")
+  ) %>% 
+    mutate(
+      outcome = str_trim(outcome),
+      ref = str_remove(ref, pattern = "\\s\\(.+")
+    ) %>%
+    separate(outcome, into = c("goals_home", "goals_away"), convert = T)
+  
+}
+
+# function for scraping results for multiple matches
 get_all_match_data <- function(
-  spielbericht_urls,
+  spielbericht_urls, # needs to be a named nested list, see body
   subset = NULL,
   sleep = 1, 
   output = "assign",
+  locale =  "de_DE.UTF-8",
   path = paste0(getwd(), "match_data.Rds")
-  ) {
+) {
   
   if(!is.null(subset)) spielbericht_urls <- spielbericht_urls[subset]
   
@@ -52,7 +115,7 @@ get_all_match_data <- function(
         ~ {
           urls <- spielbericht_urls[[x]] %>% unlist
           cat("Getting match data for '", league_names[x], "' ", ": match ", .x, "\n", sep = " ")
-          get_match_data(urls[.x], sleep = sleep) 
+          get_match_data(urls[.x], sleep = sleep, locale) 
         }
       ) %>% reduce(rbind)
       
@@ -71,7 +134,7 @@ get_all_match_data <- function(
 
 
 ##########################################################################################
- 
+
 all_spielplaene <- list(
   "A League (Australia)" = c(
     paste0(base_url, "/spielplan/aus-a-league-", "2019-2020", "-spieltag/", 1:29, "/"),
@@ -139,14 +202,14 @@ all_spielplaene <- list(
   ),
   "Ekstraklasa (Poland)" = c(
     paste0(base_url, "/spielplan/pol-ekstraklasa-", "2019-2020", "-spieltag/", 1:30, "/"),
-    paste0(base_url, "/spielplan/ita-ekstraklasa-", "2020-2021", "-spieltag/", 1:30, "/")
+    paste0(base_url, "/spielplan/pol-ekstraklasa-", "2020-2021", "-spieltag/", 1:30, "/")
   ),
   "Primera Liga (Portugal)" = c(
     paste0(base_url, "/spielplan/por-primeira-liga-", "2019-2020", "-spieltag/", 1:34, "/"),
     paste0(base_url, "/spielplan/por-primeira-liga-", "2020-2021", "-spieltag/", 1:34, "/")
   ),
   "Liga 1 (Romania)" = c(
-    paste0(base_url, "/spielplan/rou-liga-1-", "2019-2020", "-spieltag/", 1:30, "/"),
+    paste0(base_url, "/spielplan/rou-liga-1-", "2019-2020", "-spieltag/", 1:26, "/"),
     paste0(base_url, "/spielplan/rou-liga-1-", "2020-2021", "-spieltag/", 1:30, "/")
   ),
   "SuperLiga (Serbia)" = c(
@@ -173,7 +236,8 @@ all_spielplaene <- list(
 )
 
 # scrape / generate spielbericht urls
-all_spielbericht_urls(spielplaene = all_spielplaene)
+# all_spielbericht_urls(spielplaene = all_spielplaene)
+spielbericht_urls <- readRDS("spielbericht_urls.RDS")
 
 # scrape and generate match data
 
@@ -184,10 +248,34 @@ scrapes$`AUS-ALeague` <- get_all_match_data(spielbericht_urls, subset = "A Leagu
 scrapes$`ALB-KategoriaSuperiore` <- get_all_match_data(spielbericht_urls, subset = "Kategoria Superiore (Albania)", output = "return")
 scrapes$`AUT-Bundesliga` <- get_all_match_data(spielbericht_urls, subset = "1. Bundesliga (Austria)", output = "return")
 scrapes$`AUT-Liga2` <- get_all_match_data(spielbericht_urls, subset = "2. Liga (Austria)", output = "return")
-
 scrapes$`CRC-PrimeraDivision` <- get_all_match_data(spielbericht_urls, subset = "Primera Division - Apertura (Costa Rica)", output = "return")
 scrapes$`CRC-PrimeraDivisionClausura` <- get_all_match_data(spielbericht_urls, subset = "Primera Division - Clausura (Costa Rica)", output = "return")
 scrapes$`DEN-Superligaen` <- get_all_match_data(spielbericht_urls, subset = "Superligaen (Denmark)", output = "return")
 scrapes$`ENG-PremierLeague` <- get_all_match_data(spielbericht_urls, subset = "Premier League (England)", output = "return")
 scrapes$`ENG-Championship` <- get_all_match_data(spielbericht_urls, subset = "Championship (England)", output = "return")
 scrapes$`GER-Bundesliga1` <- get_all_match_data(spielbericht_urls, subset = "1. Bundesliga (Germany)", output = "return")
+scrapes$`GER-Bundesliga2` <- get_all_match_data(spielbericht_urls, subset = "2. Bundesliga (Germany)", output = "return")
+scrapes$`GER-3Liga` <- get_all_match_data(spielbericht_urls, subset = "3. Liga (Germany)", output = "return")
+scrapes$`GRE-SuperLeague` <-  get_all_match_data(spielbericht_urls, subset = "Super League (Greece)", output = "return")
+scrapes$`HUN-OTBPankLiga` <-  get_all_match_data(spielbericht_urls, subset = "OTP Bank Liga (Hungary)", output = "return")
+
+scrapes$`ITA-SerieA` <-  get_all_match_data(spielbericht_urls, subset = "Serie A (Italy)", output = "return")
+scrapes$`ITA-SerieB` <-  get_all_match_data(spielbericht_urls, subset = "Serie B (Italy)", output = "return")
+scrapes$`POL-Ekstraklasa` <-  get_all_match_data(spielbericht_urls, subset = "Ekstraklasa (Poland)" , output = "return")
+scrapes$`POR-PrimeiraLiga` <-  get_all_match_data(spielbericht_urls, subset = "Primera Liga (Portugal)" , output = "return")
+scrapes$`ROU-Liga1` <-  get_all_match_data(spielbericht_urls, subset = "Liga 1 (Romania)", output = "return")
+
+scrapes$`SRB-SuperLiga`  <-  get_all_match_data(spielbericht_urls, subset = "SuperLiga (Serbia)" , output = "return")
+scrapes$`SVN-PrvaLiga` <-  get_all_match_data(spielbericht_urls, subset = "PrvaLiga (Slovenia)" , output = "return")
+scrapes$`ESP-LaLiga` <-  get_all_match_data(spielbericht_urls, subset = "La Liga (Spain)", output = "return")
+scrapes$`ESP-SegundaDivision` <- get_all_match_data(spielbericht_urls, subset = "Segunda Division (Spain)", output = "return")
+scrapes$`UKR-PremyerLiga` <- get_all_match_data(spielbericht_urls, subset = "Premyer Liga (Ukraine)", output = "return")
+
+# save raw results
+saveRDS(scrapes, file = "all_matches.RDS")
+
+# join results
+wfb_data <- map(scrapes, ~ .[[1]]) %>% reduce(rbind)
+saveRDS(wfb_data, file = "wfb_data.RDS")
+
+
